956380f84db351713d1203e0d20cbf3687b7c446
AhmedFathiHassan <ahmedfathi010223@gmail.com>
2025-02-02 16:52:17 +0200

    Adjust wrok and late hours to include early and late grace

--- erpnext_custom/erpnext_custom/doctype/custom_attendance/custom_attendance.py
+++ erpnext_custom/erpnext_custom/doctype/custom_attendance/custom_attendance.py
@@ -13,36 +13,31 @@ class customAttendance(Document):
 
     def get_work_hours(self):
         if self.check_out and self.check_in:
-            # Get All Values From Doctypes And Convert It In Time Format
             start_time = datetime.strptime(str(frappe.db.get_single_value('Attendance Setting', 'start_time')), '%H:%M:%S')
             end_time = datetime.strptime(str(frappe.db.get_single_value('Attendance Setting', 'end_time')), '%H:%M:%S')
-            late_entry_grace_period = frappe.db.get_single_value('Attendance Setting', 'late_entry_grace_period')  # فترة السماح للتأخير (بالدقائق)
-            early_exit_grace_period = frappe.db.get_single_value('Attendance Setting', 'early_exit_grace_period')  # فترة السماح للخروج المبكر (بالدقائق)
+            late_entry_grace_period = frappe.db.get_single_value('Attendance Setting', 'late_entry_grace_period')  
+            early_exit_grace_period = frappe.db.get_single_value('Attendance Setting', 'early_exit_grace_period') 
             check_in = datetime.strptime(self.check_in, '%H:%M:%S')
             check_out = datetime.strptime(self.check_out, '%H:%M:%S')
 
-            # حساب التأخير
-            late_entry_delta = (check_in - start_time).total_seconds() / 60  # الفرق بالدقائق
+            late_entry_delta = (check_in - start_time).total_seconds() / 60  
             if late_entry_delta <= late_entry_grace_period:
-                late_entry_delta = 0  # لو التأخير أقل من فترة السماح، التأخير بيكون صفر
+                late_entry_delta = 0 
             else:
-                late_entry_delta = late_entry_delta - late_entry_grace_period  # لو التأخير أكثر من فترة السماح، نحسب الفرق
+                late_entry_delta = late_entry_delta - late_entry_grace_period  
 
-            # حساب الخروج المبكر
-            early_exit_delta = (end_time - check_out).total_seconds() / 60  # الفرق بالدقائق
+            early_exit_delta = (end_time - check_out).total_seconds() / 60 
             if early_exit_delta <= early_exit_grace_period:
-                early_exit_delta = 0  # لو الخروج المبكر أقل من فترة السماح، بيكون صفر
+                early_exit_delta = 0  
             else:
-                early_exit_delta = early_exit_delta - early_exit_grace_period  # لو الخروج المبكر أكثر من فترة السماح، نحسب الفرق
+                early_exit_delta = early_exit_delta - early_exit_grace_period 
 
-            # تحويل التأخير والخروج المبكر لساعات
-            self.late_hours = (late_entry_delta + early_exit_delta) / 60  # إجمالي التأخير بالساعات
+            self.late_hours = (late_entry_delta + early_exit_delta) / 60
 
-            # إزالة الكسور العشرية من ساعات التأخير
-            self.late_hours = int(self.late_hours)  # إزالة الكسور العشرية
 
-            # حساب عدد ساعات العمل الفعلية
-            self.work_hours = 8 - self.late_hours  # عدد ساعات العمل الفعلية
+            self.late_hours = int(self.late_hours) 
+
+            self.work_hours = 8 - self.late_hours
 
             frappe.msgprint(f'Attendance For <b>{self.employee_name}</b> inserted successfully')
 
@@ -50,10 +45,8 @@ class customAttendance(Document):
             frappe.throw("Attendance Date, Check In, and Check Out are required")
 
     def update_status(self):
-        # فترة السماح للتأخير أو الخروج المبكر (30 دقيقة)
         grace_period_minutes = 30
 
-        # لو التأخير أو الخروج المبكر أكثر من 30 دقيقة، يبقى غياب
         if self.late_hours * 60 > grace_period_minutes:
             self.status = "Absent"
         else:
